25364425e5c9dbf47ef3a06565f5e0e9e53f320b
wesinator <5124946+wesinator@users.noreply.github.com>
2019-12-27 13:10:28 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # indicator-deprecation-calculator
 Web app to calculate indicator confidence deprecation timelines (used with threat intel platforms such as ThreatConnect).
+Given starting confidence/severity value, deprecation scheme, and total days, calculates the final value after deprecation.
+
+For more information about indicator deprecation, see https://www.slideshare.net/ThreatConnect/managing-indicator-deprecation-in-threatconnect
+
+Built using [Foundation](https://get.foundation)
---
+++ deprecation.js
@@ -0,0 +1,31 @@
+// get values specified in HTML for deprecation calc function
+function getDeprecation() {
+    var startingValue = document.getElementById("start").value;
+
+    var deprecationAmount = document.getElementById("deprecation-amount").value;
+    var deprecationIntervalDays = document.getElementById("interval-days").value;
+
+    var deprecationPeriodDays = document.getElementById("period-days").value;
+
+    var endValue = deprecationCalc(startingValue, deprecationAmount, deprecationIntervalDays, deprecationPeriodDays);
+    console.log("Deprecation end value", endValue);
+
+    var output = document.getElementById("end-value");
+    //console.log(output.innerText);
+    output.innerText += endValue;
+    document.getElementById("results").removeAttribute("hidden");
+}
+
+/*
+    Calculate the deprecation given:
+        starting value
+        deprecation (i.e. subtract) amount
+        number of days per deprecation cycle
+        total period of deprecation, in days
+*/
+function deprecationCalc(startingValue, deprecationAmount, intervalDays, periodDays) {
+    var endValue = startingValue;
+    endValue = startingValue - deprecationAmount * (periodDays/intervalDays);
+
+    return endValue;
+}
